8e577975a10bcfe2676dfbbd9075293e1a18ae7e
zjamesdandrea <zjamesdandrea@gmail.com>
2025-04-16 16:55:36 -0500

    Updated the file

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ chocolates = [
         "name": "White Chocolate",
         "image": "https://m.media-amazon.com/images/I/61-pg5u+9pL.jpg",
         "price": 6.49
+    },
+    {
+        "name": "Cream Chocolate",
+        "image": "https://m.media-amazon.com/images/I/61-pg5u+9pL.jpg",
+        "price": 15.49
     }
 ]
 
